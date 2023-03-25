e575d405974df840492e9c232339b10d0a653f4e
John1Souza <109979241+John1Souza@users.noreply.github.com>
2023-03-25 19:38:40 -0300

    Some nodejs, typescript concepts

--- index.js
+++ index.js
@@ -23,7 +23,6 @@
 //   return primes;
 // };
 
-
 // const quota = document.querySelector('#quota')
 // const output = document.querySelector('#output')
 
@@ -36,7 +35,6 @@
 // document.getElementById('reload').addEventListener('click', () => {
 //     document.location.reload();
 // })
-
 
 // Events
 
@@ -61,20 +59,19 @@
 //     })
 // })
 
-
 // Fetch API
 
 // const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
 
 // console.log(fetchPromise)
 
-// fetchPromise.then((response) => 
+// fetchPromise.then((response) =>
 //     console.log(`Received response: ${response.status}`)
 // )
 
 // console.log("Started request...")
 
-// const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json') 
+// const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
 
 // console.log(fetchPromise)
 
@@ -85,8 +82,7 @@
 //     })
 // })
 
-// const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json') 
-
+// const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
 
 // fetchPromise
 //     .then((response) => {
@@ -99,11 +95,9 @@
 //     console.log(data[0].name);})
 //     .catch((error) => console.log(`Could not get products ${error}`))
 
-
 // const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
 // const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found');
 // const fetchPromise3 = fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
-
 
 // Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
 //   .then((responses) => {
@@ -114,13 +108,11 @@
 //   .catch((error) => {
 //     console.error(`Failed to fetch: ${error}`)
 //   });
-
 
 //   const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
 // const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found');
 // const fetchPromise3 = fetch('bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
 
-
 // Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
 //   .then((responses) => {
 //     for (const response of responses) {
@@ -130,7 +122,6 @@
 //   .catch((error) => {
 //     console.error(`Failed to fetch: ${error}`)
 //   });
-
 
 // const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
 // const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found');
@@ -144,24 +135,174 @@
 //     console.error(`Failed to fetch: ${error}`)
 //   });
 
+// async function fetchProducts() {
+//     try {
+//         // after this line, our function will wait for the `fetch()` call to be settled
+//         // the `fetch()` call will either return a Response or throw an error
+//         const response = await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+//         if (response.ok){
+//             throw new Error(`HTTP error: ${response.status}`)
+//         }
+//         // after this line, our function will wait for the `response.json()` call to be settled
+//         // the `response.json()` call will either return the parsed JSON object or throw an error
+//         const data = await response.json()
+//         console.log(data[0].name)
+//     }
+//     catch(error) {
+//         console.error(`Could not get products: ${error}`)
+//     }
 
-async function fetchProducts() {
-    try {
-        // after this line, our function will wait for the `fetch()` call to be settled
-        // the `fetch()` call will either return a Response or throw an error
-        const response = await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
-        if (response.ok){
-            throw new Error(`HTTP error: ${response.status}`)
-        }
-        // after this line, our function will wait for the `response.json()` call to be settled
-        // the `response.json()` call will either return the parsed JSON object or throw an error
-        const data = await response.json()
-        console.log(data[0].name)
-    }
-    catch(error) {
-        console.error(`Could not get products: ${error}`)
-    }
+// }
 
+// fetchProducts()
+
+// function init() {
+//     var name = "Mozila"
+//     function displayName(){
+//         // displayName() is the inner function, that forms the closure
+//         console.log(name) // use variable declared in the parent function
+//     }
+//     displayName()
+// }
+
+// init()
+
+// const counter = (
+//     function() {
+//         let privateCounter = 0
+//         function changeBy(val){
+//             privateCounter += val
+//         }
+//         return {
+//             increment(){
+//                 changeBy(1)
+//             },
+//             decrement(){
+//                 changeBy(-1)
+//             },
+//             value(){
+//                 return privateCounter
+//             },
+//         };
+//     }
+// )();
+
+// console.log(counter.value()); // 0.
+
+// counter.increment();
+// counter.increment();
+// console.log(counter.value()); // 2
+
+// counter.decrement();
+// console.log(counter.value()); // 1
+
+// const makeCounter = () => {
+//     let privateCounter = 0;
+//     function changeBy(val){
+//         privateCounter += val
+//     }
+
+//     return {
+//         increment(){
+//             changeBy(1)
+//         },
+//         decrement(){
+//             changeBy(-1)
+//         },
+//         value(){
+//             return privateCounter
+//         },
+//     };
+// };
+
+// const counter1 = makeCounter()
+// const counter2 = makeCounter()
+
+// console.log(counter1.value()); // 0.
+
+// counter1.increment()
+// counter1.increment()
+// console.log(counter1.value()); // 2.
+
+// counter1.decrement()
+// console.log(counter1.value()); // 1.
+// console.log(counter2.value()); // 0.
+
+// global scope
+
+// const e = 10;
+// function sum(a){
+//     return function (b){
+//         return function (c){
+//             // outer functions scope
+//             return function (d){
+//                 // local scope
+//                 return a + b + c + d + e
+//             }
+//         }
+//     }
+// }
+
+// console.log(sum(1)(2)(3)(4)); // 20
+
+// global scope
+
+// const f = 10;
+// function sum(a) {
+//     return function sum2(b){
+//         return function sum3(c){
+//             // outer functions scope
+//             return function sum4(d){
+//                 // local scope
+//                 return a + b + c + d + e
+//             }
+//         }
+//     }
+// }
+
+// const sum2 = sum(1)
+// const sum3 = sum2(2)
+// const sum4 = sum3(3)
+// const result = sum4(4)
+// console.log(result); // 20
+
+// function outer() {
+//     const x = 5;
+//     if (Math.random() > 0.5){
+//         const y = 6;
+//         return () => console.log(x, y);
+//     }
+// }
+
+// outer()();
+
+// import { getX , setX } from "./myModule.js"
+
+// console.log(getX())
+// setX(6)
+// console.log(getX())
+
+function showHelp(help) {
+  document.getElementById("help").textContent = help;
+}
+const makeHelpCallback = (help) => {
+  return function () {
+    showHelp(help);
+  };
+};
+function setupHelp() {
+  var helpText = [
+    { id: "email", help: "Your email address" },
+    { id: "name", help: "Your full name" },
+    { id: "age", help: "Your age (you must be over 16)" },
+  ];
+
+  for (var i = 0; i < helpText.length; i++) {
+    // Culprit is the use of `var` on this line
+    var item = helpText[i];
+    document.getElementById(item.id).onfocus =
+      makeHelpCallback(item.help);
+  }
 }
 
-fetchProducts()
+setupHelp();
